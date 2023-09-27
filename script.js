ff92f1b088a808f59a9f2338ca0817ce7b5b9b88
quanta2k <72522871+quanta2k@users.noreply.github.com>
2023-09-27 15:27:07 +0000

    ..

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-/*
-  Rock Paper Scissors 🚀🔥
-  Concepts covered in this project
-    👉 For loops
-    👉 Dom Manipulation
-    👉 Variables
-    👉 Conditionals (if else if)
-    👉 Template Literals
-    👉 Event Listeners
-    👉 Higher order Function (Math.random())
-*/
+
 
 const totalScore={computerScore:0,playerScore:0}
 
-// ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
-// getComputerChoice() 👉 'Rock'
-// getComputerChoice() 👉 'Scissors'
 function getComputerChoice() {
   const rpsChoice=['Rock','Paper','Scirrors']
   const randomNumber= Math.floor(Math.random()*3)
   return rpsChoice[randomNumber]
 }
 
-// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
-// human wins - getResult('Rock', 'Scissors') 👉 1
-// human loses - getResult('Scissors', 'Rock') 👉 -1
-// human draws - getResult('Rock', 'Rock') 👉 0
 function getResult(playerChoice, computerChoice) {
-  // return the result of score based on if you won, drew, or lost
 
   let score;
-  // All situations where human draws, set `score` to 0
   if(playerChoice==computerChoice){
     score=0
   }
 
-  // All situations where human wins, set `score` to 1
   else if(playerChoice== 'Rock' && computerChoice=='Scissors'){
     score=1
   }
@@ -44,19 +24,14 @@ function getResult(playerChoice, computerChoice) {
   else if(playerChoice== 'Scissors' && computerChoice=='Paper'){
     score=1
   }
-  // make sure to use else ifs here
-  
-
-  // Otherwise human loses (aka set score to -1)
   else{
     score=-1
   }
 
-  // return score
   return score
 }
 
-// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
+
 function showResult(score, playerChoice, computerChoice) {
   
   
@@ -78,23 +53,19 @@ function showResult(score, playerChoice, computerChoice) {
   playerScoreDiv.innerText=totalScore['playerScore']
 }
 
-// ** Calculate who won and show it on the screen **
 function onClickRPS(playerChoice) {
   console.log({playerChoice})
   const computerChoice=getComputerChoice()
   console.log({computerChoice})
   const score=getResult(playerChoice,computerChoice)
   totalScore['playerScore'] +=score;
-  // totalScore['computerScore'] +=score
   console.log({score})
   console.log(totalScore)
   showResult(score,playerChoice,computerChoice)
 }
 
 
-// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
 function playGame() {
-  // use querySelector to select all RPS Buttons
   const rpsButtons=document.querySelectorAll('.rpsButton')
   rpsButtons[0].onclick=()=>console.log(rpsButtons[0].value)
   
@@ -107,7 +78,6 @@ function playGame() {
   
 }
 
-// ** endGame function clears all the text on the DOM **
 function endGame(totalScore) {
   totalScore['playerScore']=0
   totalScore['computerScore']=0
